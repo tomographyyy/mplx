acb915fdf8950f0d76a38c54bb045925b688b400
Tomohiro TAKAGAWA <34155315+tomographyyy@users.noreply.github.com>
2024-04-03 15:31:23 +0900

    Update setup.py for install requirements

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from setuptools import setup, find_packages
 
+def load_requires_from_file(filepath):
+    with open(filepath) as fp:
+        return [pkg_name.strip() for pkg_name in fp.readlines()]
+
 setup(
     name='mplx',  # name of package shown by "pip list"
     version="0.0.3",
     description="matplotlib extension package",
     author='Tomohiro TAKAGAWA',
     packages=find_packages(),  # define list of modules
-    license='Apache-2.0'
+    license='Apache-2.0',
+    install_requires=load_requires_from_file("requirements.txt")
 )
